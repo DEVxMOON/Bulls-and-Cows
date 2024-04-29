e80f7454e13ed659d197957f89d2fc8edcd22115
HR <dev.muun07@gmail.com>
2024-04-29 15:43:49 +0900

    :sparkles:[Feat] 숫자야구 필수 구현 기능 - 구현 완료

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,55 @@
+package hr.bullsAndCows
+
+fun main() {
+
+    val size = 3
+    val answer = makeAnswer(size)
+
+    while (true) {
+        println("숫자를 입력하세요")
+        val numbers = readln().toInt()
+        val numberSheet = numbers.toString().map { it.toString().toInt() }.toIntArray()
+
+        if (numberSheet.size != size) {
+            println("범위 외 입력입니다.")
+            continue
+        } else if (hasDuplicates(numberSheet)) {
+            println("중복된 숫자가 있습니다.")
+            continue
+        } else {
+            val result = checkGuess(answer, numberSheet)
+            println(result)
+            if (result == "3S") {
+                println("정답!")
+                break
+            }
+        }
+    }
+}
+
+fun makeAnswer(size: Int): IntArray = (1..9).toList().shuffled().take(size).toIntArray()
+
+fun hasDuplicates(arr: IntArray): Boolean {
+    return arr.size != arr.distinct().size
+}
+
+fun checkGuess(answer: IntArray, guess: IntArray): String {
+    var strikes = 0
+    var balls = 0
+
+    for (i in answer.indices) {
+        if (answer[i] == guess[i]) {
+            strikes++
+        } else if (answer.contains(guess[i])) {
+            balls++
+        }
+    }
+
+    return when {
+        strikes == 3 -> "3S"
+        strikes > 0 && balls > 0 -> "${strikes}S ${balls}B"
+        strikes > 0 -> "${strikes}S"
+        balls > 0 -> "${balls}B"
+        else -> "OUT"
+    }
+}
